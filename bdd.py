fca27d594ec439dd6a563916deeab9d19d1db9bb
Timtonix <timtonic@outlook.fr>
2024-02-18 16:30:44 +0100

    update bdd and create scraper api

--- bdd.py
+++ bdd.py
@@ -5,14 +5,18 @@ class International:
     def __init__(self):
         self.con = sqlite3.connect("international.db")
         self.cursor = self.con.cursor()
-        self.execute(
-            """CREATE TABLE IF NOT EXISTS journeaux(date DATE NOT NULL, lien TEXT NOT NULL)"""
+        self.cursor.execute(
+            """CREATE TABLE IF NOT EXISTS journeaux(date TEXT NOT NULL, lien TEXT NOT NULL, timestamp INTEGER NOT NULL)"""
         )
 
         self.con.commit()
 
-    def insert_journeaux(self, date, lien):
-        self.cursor.execute("INSERT INTO journeaux VALUES (?,?)", (date, lien))
+    def insert_journeaux(self, date, lien, timestamp):
+        self.cursor.execute("INSERT INTO journeaux VALUES (?,?, ?)", (date, lien, timestamp))
+        self.con.commit()
+
+    def get_journeaux(self):
+        self.cursor.execute("SELECT * FROM journeaux ORDER BY timestamp")
 
 
 
